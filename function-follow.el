7fc8b71be8d32004e01e8429b18a7a1bcefbfb64
Andrea Soliz <and.sol17@gmail.com>
2016-05-19 13:31:18 -0500

    added foundation for other language support

--- function-follow.el
+++ function-follow.el
@@ -1,10 +1,27 @@
+(defvar ff/java-definition-keywords '("public" "private"))
+
 (defun ff/function-follow (point-mark mark-region)
-  "Find the definition of the selected function"
+  "Find the definition of the selected/highlighted function"
   (interactive "r")
   (if (or (char-equal ?\( (char-after mark-region)) 
           (char-equal ?\( (char-after (1- mark-region))))
-          (let ((function (replace-regexp-in-string
-                           " " "" (buffer-substring-no-properties point-mark mark-region))))
-            (message "concat %s" (re-search-forward 
-                                  (concat (concat "[(private)(static)(public)]* " function) "*")))))
+      (let ((regex 
+             (ff/get-major-mode-keywords
+              (buffer-substring-no-properties point-mark mark-region))))
+        (if (or (re-search-backward regex nil t) (re-search-forward regex nil t))
+            (beginning-of-line))))
   (deactivate-mark))
+
+(defun ff/assemble-regex (function mode)
+  "Assemble the regex for Java"
+  (let ((rx "\\(") (list))
+    (dolist (element ff/java-definition-keywords list)
+      (setq rx (concat rx (mapconcat 'identity (cons element list) " ") "\\|")))
+    (concat (substring rx 0 -2) "\\).*" (replace-regexp-in-string " " "" function))))
+
+(defun ff/get-major-mode-keywords (function)
+  "Get the keywords for the major mode
+   to assemble the regex"
+  (pcase major-mode
+    (`java-mode    (ff/assemble-regex function ff/java-definition-keywords)) 
+    (_             (message "Not a supported major mode"))))
